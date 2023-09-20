46ab1d892ce3d0602e03731d1ee4e1eac82b65b8
Valmur Prado <valmurprado@pop-os.localdomain>
2023-09-20 20:13:42 +0100

    adjust terraform code

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -1,9 +1,3 @@
-variable "random_number" {
-    type        = number
-    description = "A random number to identify resources"
-    default     = 0
-}
-
 variable "location" {
   type        = string
   default     = "westeurope"
